c0f5938e2d2a106d4e00634e3956ebdfcaffb385
Dioselyn Colina <dioze09@gmail.com>
2022-05-12 20:00:47 -0500

    Update App

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import TableCoins from './components/TableCoins';
-
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import TableCoins from "./components/TableCoins";
 
 function App() {
-
   const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const getData = async () => {
-  const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1");
-  //console.log(res.data); 
-  setCoins(res.data);
-}
- 
-  useEffect(() => { 
-  getData()      
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1"
+    );
+    //console.log(res.data);
+    setCoins(res.data);
+  };
+
+  useEffect(() => {
+    getData();
   }, []);
- 
 
   return (
     <React.Fragment>
-    <div className="container">
-      <div className="row">
-      <h1 className="text-center mt-4">API Crypto Coingecko</h1>
-      <input type="search" placeholder='Search a Coin' className='form-control bg-dark text-align border-0 mt-4 text-center text-white' onChange={(event) => setSearch(event.target.value)} ></input>
-      <TableCoins coins={coins} search={search}/>
+      <div className="container">
+        <div className="row">
+          <h1 className="text-center mt-4">API Crypto Coingecko</h1>
+          <div className="col"></div>
+          <div className="col-md-6">
+            <input
+              type="search"
+              placeholder="Search a Coin"
+              className="form-control bg-dark text-align border-0 mt-4 text-center text-white"
+              onChange={(event) => setSearch(event.target.value)}
+            ></input>
+          </div>
+          <div className="col"></div>
+
+          <TableCoins coins={coins} search={search} />
+        </div>
       </div>
-    </div>
-    <footer>
-    <p className='text-center text-muted'>Powered By <a href="https://github.com/dioselyn" className="text-decoration-none" target="_blank" rel='noreferrer'>Dioselyn Colina</a></p></footer>
-    </React.Fragment> 
+      <footer>
+        <p className="text-center text-muted">
+          Powered By{" "}
+          <a
+            href="https://github.com/dioselyn"
+            className="text-decoration-none"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dioselyn Colina
+          </a>
+        </p>
+      </footer>
+    </React.Fragment>
   );
 }
 
